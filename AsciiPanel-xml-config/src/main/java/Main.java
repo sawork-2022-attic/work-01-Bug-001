1a867fcd71dcd8627b564beba69c71a2ca3bd9b9
Bug-001 <191850169@smail.nju.edu.cn>
2022-02-24 20:46:53 +0800

    complete xml config

---
+++ AsciiPanel-xml-config/src/main/java/Main.java
@@ -0,0 +1,27 @@
+import javax.swing.JFrame;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import asciiPanel.AsciiPanel;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
+        AsciiPanel asciiPanel = context.getBean("panel", AsciiPanel.class);
+        asciiPanel.setSize(800, 240);
+
+        for (int i = 0; i < 24; i++) {
+            asciiPanel.write("Hello, world!", 0, i);
+        }
+        JFrame frame = new JFrame("AsciiPanel");
+        frame.add(asciiPanel);
+        frame.setSize(80 * 9, 24 * 16);
+        frame.setVisible(true);
+
+        ((ClassPathXmlApplicationContext) context).close();
+    }
+}
